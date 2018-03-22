54020cad7383d5c7f2cea48a1128022ac9165d34
Martin Flores <mf34314@gmail.com>
2018-03-22 15:28:12 -0500

    Initial commit

---
+++ desktop/src/com/asteroids/team/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.asteroids.team.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.asteroids.team.Asteroids;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Asteroids(), config);
+	}
+}
