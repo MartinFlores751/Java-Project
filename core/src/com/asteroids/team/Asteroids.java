fc6ead10e3090d291b356d06ef2c3315ed7bd8a1
MartinFlores751 <mf34314@gmail.com>
2018-03-22 17:43:25 -0500

    Pics added

--- core/src/com/asteroids/team/Asteroids.java
+++ core/src/com/asteroids/team/Asteroids.java
@@ -2,18 +2,21 @@ package com.asteroids.team;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Asteroids extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+	private SpriteBatch batch;
+	private Texture img;
+	private Music moosic;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		//moosic = new newMusic();
 	}
 
 	@Override
